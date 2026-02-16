7b3ffda135ed34ad864b22e97487e3cb68ad70ef
varya <varya123@gmail.com>
2026-02-16 10:56:05 +0500

    dz

---
+++ gitBush/c#/code/30.12.25/square.cs
@@ -0,0 +1,13 @@
+using System;
+
+public static void DrawSquare(int sideLength, char symbol)
+{
+    for (int i = 0; i < sideLength; i++)
+    {
+        for (int j = 0; j < sideLength; j++)
+        {
+            Console.Write(symbol);
+        }
+        Console.WriteLine();
+    }
+}
